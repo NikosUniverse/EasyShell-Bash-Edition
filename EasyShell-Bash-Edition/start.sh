7a4da3ea9fbccea0f59626ee4aca5fceea733ef0
Nikos Universe <75520871+NikosUniverse@users.noreply.github.com>
2020-12-10 17:32:24 +0100

    Main Files for EasyShell-Bash-Edition

---
+++ EasyShell-Bash-Edition/start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# INFORMATION! YOU DON'T NEED THIS FILE, YOU CAN ALTERNATIVELY DIRECTLY RUN ./shell.sh!
+# I AM FULLY AWARE OF THIS!
+# I JUST DID IT TO MAKE IT EASIER FOR PEOPLE WHO ARE OVERWHELMED WITH MANY FILES!
+
+# Nikos Easy Shell
+# EasyShell-Bash-Edition
+# Made with .sh shellscript
+# Author: NikosUniverse
+
+cd ./Files
+./shell.sh
